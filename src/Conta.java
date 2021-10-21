fff3f14747308a26c0ac37106fa77eab2e6e3f1f
viniciuspod <viniciuspodantas@gmail.com>
2021-10-21 11:56:43 -0300

    colocando privado em tudo

--- src/Conta.java
+++ src/Conta.java
@@ -2,9 +2,9 @@
 
 public class Conta {
 	private double saldo;
-	int agencia;
-	int numero;
-	Cliente titular;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
 	
 	public void deposita(double valor) {
 		this.saldo += valor;
@@ -33,4 +33,29 @@ public class Conta {
 	public double getSaldo() {
 		return this.saldo;
 	}
+	
+	public int getNumero() {
+		return this.numero;
+	}
+	
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	public int getAgencia() {
+		return this.agencia;
+	}
+	
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	
+	public Cliente getTitular() {
+		return this.titular;
+	}
+	
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	
 }
